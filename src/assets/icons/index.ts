eaa196773f27700e9019ddd598ba4fb8574c5100
Gustavo França <gstvz.franca@gmail.com>
2022-03-28 14:23:00 -0300

    feat: added arrow svg to assets

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,8 +1,10 @@
+import arrow from "./arrow.svg";
 import arrow_up from "./arrow_up.svg";
 import calendar from "./calendar.svg";
 import map from "./map.svg";
 import search from "./search.svg";
 
+export const icon_arrow = arrow;
 export const icon_arrow_up = arrow_up;
 export const icon_calendar = calendar;
 export const icon_map = map;
