f2aec782b9d30e08db3027f70a5775a30921fc22
Gustavo França <gstvz.franca@gmail.com>
2022-03-28 15:42:01 -0300

    feat: implemented useEffect on useCarDetails to run findChosenCar function to find the selected car and return its details to the CarDetails component

--- src/components/CarDetails/index.tsx
+++ src/components/CarDetails/index.tsx
@@ -1,35 +1,33 @@
 import * as S from "./styles";
-import { useCars } from "@shared/hooks";
 import { Button } from "@components";
 import { useCarDetails } from "./useCarDetails";
 
 export const CarDetails = () => {
-  const { cars } = useCars();
-  const [handleBackToCatalog, handleBookNow] = useCarDetails();
-
+  const { car, handleBackToCatalog, handleBookNow } = useCarDetails();
+  
   return (
     <S.Container>
       <S.Car>
         <S.CarHeader>
-          <S.MakeLogo src={cars[0]?.makeLogo} />
+          <S.MakeLogo src={car?.makeLogo} />
           <S.CarInfo>
             <S.CarMakeAndModel>
-              {cars[0]?.make} {cars[0]?.model}
+              {car?.make} {car?.model}
             </S.CarMakeAndModel>
-            <S.CarPrice>${cars[0]?.price}/day</S.CarPrice>
+            <S.CarPrice>${car?.price}/day</S.CarPrice>
           </S.CarInfo>
         </S.CarHeader>
         <S.CarPhotoContainer>
           <Button text="Back to catalog" onPress={handleBackToCatalog} />
-          <S.CarPhoto src={cars[0]?.colors[0].photo} />
+          <S.CarPhoto src={car?.colors[0].photo} />
         </S.CarPhotoContainer>
         <S.ButtonContainer>
           <Button text="Book now" forward onPress={handleBookNow} />
         </S.ButtonContainer>
       </S.Car>
       <S.CarIdAndColor>
-        <S.CarId>{cars[0]?.id}</S.CarId>
-        <S.CarColor>{cars[0]?.colors[0].color}</S.CarColor>
+        <S.CarId>{car?.id}</S.CarId>
+        <S.CarColor>{car?.colors[0].color}</S.CarColor>
       </S.CarIdAndColor>
     </S.Container>
   );
--- src/components/CarDetails/useCarDetails.ts
+++ src/components/CarDetails/useCarDetails.ts
@@ -1,7 +1,13 @@
-import { useNavigate } from "react-router-dom";
+import { useCars } from "@shared/hooks";
+import { Car } from "@shared/types";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const useCarDetails = () => {
   const navigate = useNavigate();
+  const [car, setCar] = useState<Car | null>(null);
+  const { cars } = useCars();
+  const { carId } = useParams();
 
   const handleBackToCatalog = () => {
     navigate(-1);
@@ -11,5 +17,15 @@ export const useCarDetails = () => {
     return;
   };
 
-  return [handleBackToCatalog, handleBookNow];
+  useEffect(() => {
+    const findChosenCar = () => {
+      const searchCar = cars.filter((car) => car.id === Number(carId));
+      const chosenCar = searchCar[0];
+      setCar(chosenCar);
+    };
+
+    findChosenCar();
+  }, [cars, carId]);
+
+  return { car, handleBackToCatalog, handleBookNow };
 };
