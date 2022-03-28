a3031ce240e51990778e4159667e3ae6af4cd061
Gustavo França <gstvz.franca@gmail.com>
2022-03-28 14:24:23 -0300

    feat: added colors prop to Car type

--- src/shared/types/cars.ts
+++ src/shared/types/cars.ts
@@ -1,9 +1,14 @@
 export type Car = {
   id: number;
   make: string;
+  makeLogo: string;
   model: string;
   price: number;
   thumbnail: string;
+  colors: {
+    color: string;
+    photo: string;
+  }[];
 };
 
 export interface CarsState {
