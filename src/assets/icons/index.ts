936fba42285a7d39802e2bd65197c567239a4948
Gustavo França <gstvz.franca@gmail.com>
2022-03-29 10:41:37 -0300

    feat: added forbidden icon to assets

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,11 +1,13 @@
 import arrow from "./arrow.svg";
 import arrow_up from "./arrow_up.svg";
 import calendar from "./calendar.svg";
+import forbidden from "./forbidden.svg";
 import map from "./map.svg";
 import search from "./search.svg";
 
 export const icon_arrow = arrow;
 export const icon_arrow_up = arrow_up;
 export const icon_calendar = calendar;
+export const icon_forbidden = forbidden;
 export const icon_map = map;
 export const icon_search = search;
