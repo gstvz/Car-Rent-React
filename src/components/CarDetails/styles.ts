1049f3eddb558a5b209a2715c4fc9fe098f5a0f9
Gustavo França <gstvz.franca@gmail.com>
2022-03-31 09:47:29 -0300

    feat: applied responsiveness on CarDetails, by setting display flex and direction to column on Car; Setting direction to column on CarHeader; Decreasubg height, width and margin-bottom on MakeLogo; Setting margin to 0 on CarInfo; Decreasing font-size on CarMakeAndModel, CarPrice; Decreasing height and width on CarPhoto; Setting text-align on CarIdAndColor; Decreasing font-size on CarId and CarColor

--- src/components/CarDetails/styles.ts
+++ src/components/CarDetails/styles.ts
@@ -9,32 +9,61 @@ export const Container = styled.div`
 
 export const Car = styled.div`
   flex: 1;
+
+  @media(max-width: 1200px) {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+  };
 `
 
 export const CarHeader = styled.div`
   align-items: center;
   display: flex;
+
+  @media(max-width: 1200px) {
+    flex-direction: column;
+  };
 `;
 
 export const MakeLogo = styled.img`
   height: 9rem;
   width: 7rem;
+
+  @media(max-width: 1200px) {
+    height: 6rem;
+    margin-bottom: 2rem;
+    width: 4rem;
+  };
 `
 
 export const CarInfo = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 3rem;
+
+  @media(max-width: 1200px) {
+    margin: 0;
+  };
 `
 
 export const CarMakeAndModel = styled.span`
   font-size: 4rem;
   font-weight: ${({ theme }) => theme.fontWeights.regular_700};
+
+  @media(max-width: 1200px) {
+    font-size: 2.6rem;
+  };
 `
 
 export const CarPrice = styled.span`
   font-size: 3rem;
   font-weight: ${({ theme }) => theme.fontWeights.light_300};
+
+  @media(max-width: 1200px) {
+    font-size: 2rem;
+    margin: 0 auto;
+  };
 `
 
 export const CarPhotoContainer = styled.div`
@@ -46,20 +75,37 @@ export const CarPhoto = styled.img`
   height: 30rem;
   margin: 0 auto;
   width: 60rem;
+
+  @media(max-width: 1200px) {
+    height: 12rem;
+    width: 24rem;
+  };
 `;
 
 export const CarIdAndColor = styled.aside`
   width: 6rem;
+
+  @media(max-width: 1200px) {
+    text-align: center;
+  };
 `;
 
 export const CarId = styled.p`
   font-size: 4rem;
   font-weight: ${({ theme }) => theme.fontWeights.regular_700};
+
+  @media(max-width: 1200px) {
+    font-size: 2.6rem;
+  };
 `
 
 export const CarColor = styled.p`
   font-size: 3rem;
   font-weight: ${({ theme }) => theme.fontWeights.light_300};
+
+  @media(max-width: 1200px) {
+    font-size: 2.6rem;
+  };
 `
 
 export const ButtonContainer = styled.div`
