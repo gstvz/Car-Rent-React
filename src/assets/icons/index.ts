e1b08efaff093b5e582cdabb519cf8be0128165a
Gustavo França <gstvz.franca@gmail.com>
2022-03-31 09:19:43 -0300

    feat: added mobile_menu icon to assets

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,6 +3,7 @@ import arrow_up from "./arrow_up.svg";
 import calendar from "./calendar.svg";
 import forbidden from "./forbidden.svg";
 import map from "./map.svg";
+import mobile_menu from "./mobile_menu.png";
 import search from "./search.svg";
 
 export const icon_arrow = arrow;
@@ -10,4 +11,5 @@ export const icon_arrow_up = arrow_up;
 export const icon_calendar = calendar;
 export const icon_forbidden = forbidden;
 export const icon_map = map;
+export const icon_mobile_menu = mobile_menu;
 export const icon_search = search;
